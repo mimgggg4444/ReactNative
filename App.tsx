d2110f1daca5300958186079611dfbc472687ef8
MIN <66135779+mimgggg4444@users.noreply.github.com>
2024-03-13 06:36:49 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>안녕하세요, 리액트 네이티브!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
